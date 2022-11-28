89498e23d72decd8e865cfdd7f9a62c63aa99113
lucaspinarj <lucaspinarj@hotmail.com>
2022-11-28 10:36:10 -0300

    carrinho

--- angular-proway/projeto-compra/src/app/carrinho.service.ts
+++ angular-proway/projeto-compra/src/app/carrinho.service.ts
@@ -11,7 +11,17 @@ export class CarrinhoService {
   constructor() { }
 
   obtemCarrinho() {
+  return JSON.parse(localStorage.getItem("carrinho")|| "");
+  }
 
+  adicionarAoCarrinho(produto: IProdutoCarrinho) {
+    this.itens.push(produto);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens));
+  }
+
+  limparCarrinho() {
+    this.itens = [];
+    localStorage.clear();
   }
 
 }
